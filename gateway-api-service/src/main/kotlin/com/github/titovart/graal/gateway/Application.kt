b7b38199c74447aa07010c1293eaf8ceecf8a1ed
Titov Artem <tita@1c.ru>
2017-11-15 12:58:52 +0300

    Add gateway module with simple Zuul routing

---
+++ gateway-api-service/src/main/kotlin/com/github/titovart/graal/gateway/Application.kt
@@ -0,0 +1,19 @@
+package com.github.titovart.graal.gateway
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+
+
+@SpringBootApplication
+@EnableZuulProxy
+class Application {
+
+    companion object {
+
+        @JvmStatic fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
+        }
+    }
+
+}
---
+++ gateway-api-service/src/main/kotlin/com/github/titovart/graal/gateway/web/GatewayApiController.kt
@@ -0,0 +1,8 @@
+package com.github.titovart.graal.gateway.web
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
+class GatewayApiController
