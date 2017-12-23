f36886198dd8c9369faaa5eb65e3f4d959609668
titart <pbi62007@yandex.ru>
2017-12-23 20:06:32 +0300

    Fix application configs for zuul service

--- gateway-service/src/main/kotlin/com/github/titovart/graal/gateway/GatewayServiceApplication.kt
+++ gateway-service/src/main/kotlin/com/github/titovart/graal/gateway/GatewayServiceApplication.kt
@@ -2,10 +2,12 @@ package com.github.titovart.graal.gateway
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 class GatewayServiceApplication {
 
