fe83e465c23a11271de72602f1ff19902ff13d72
titart <pbi62007@yandex.ru>
2017-12-23 20:04:29 +0300

    Add JPA config for H2 database for microservices

--- hashtag-service/src/main/kotlin/com/github/titovart/graal/hashtag/HashtagServiceApplication.kt
+++ hashtag-service/src/main/kotlin/com/github/titovart/graal/hashtag/HashtagServiceApplication.kt
@@ -1,30 +1,16 @@
 package com.github.titovart.graal.hashtag
 
-import com.github.titovart.graal.hashtag.model.HashTag
-import com.github.titovart.graal.hashtag.service.HashTagService
-import org.slf4j.LoggerFactory
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
-import org.springframework.context.annotation.Bean
 
 @EnableEurekaClient
 @SpringBootApplication
 class HashtagServiceApplication {
 
-    private val logger = LoggerFactory.getLogger(this.javaClass)
-
-    @Bean
-    fun init(service: HashTagService) = CommandLineRunner {
-        service.save(HashTag("like4like"))
-        service.save(HashTag("insta_mom"))
-
-        logger.info("Hash tag micro-service has started")
-    }
-
     companion object {
-        @JvmStatic fun main(args: Array<String>) {
+        @JvmStatic
+        fun main(args: Array<String>) {
             SpringApplication.run(HashtagServiceApplication::class.java, *args)
         }
     }
--- post-service/src/main/kotlin/com/github/titovart/graal/post/PostServiceApplication.kt
+++ post-service/src/main/kotlin/com/github/titovart/graal/post/PostServiceApplication.kt
@@ -1,23 +1,12 @@
 package com.github.titovart.graal.post
 
-import com.github.titovart.graal.post.model.Post
-import com.github.titovart.graal.post.service.PostService
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 @EnableDiscoveryClient
 class PostServiceApplication {
-
-    @Bean
-    fun init(service: PostService) = CommandLineRunner {
-        service.save(Post("Post content #1", "Caption #1", 1, mutableSetOf(1L, 2L)))
-        service.save(Post("Post content #2", "Caption #2", 1))
-        service.save(Post("Post content #3", "Caption #3", 2))
-    }
 
     companion object {
 
