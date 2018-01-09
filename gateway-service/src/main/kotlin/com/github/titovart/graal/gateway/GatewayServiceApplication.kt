c024bb9f696cf12165dd97c6cfe96a03ba016acc
titart <pbi62007@yandex.ru>
2018-01-10 01:01:09 +0300

    Add CORS filter to gateway service

--- gateway-service/src/main/kotlin/com/github/titovart/graal/gateway/GatewayServiceApplication.kt
+++ gateway-service/src/main/kotlin/com/github/titovart/graal/gateway/GatewayServiceApplication.kt
@@ -4,12 +4,34 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
+import org.springframework.context.annotation.Bean
+import org.springframework.web.filter.CorsFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
 class GatewayServiceApplication {
+
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.addAllowedOrigin("*")
+        config.addAllowedHeader("*")
+        config.addAllowedMethod("OPTIONS")
+        config.addAllowedMethod("HEAD")
+        config.addAllowedMethod("GET")
+        config.addAllowedMethod("PUT")
+        config.addAllowedMethod("POST")
+        config.addAllowedMethod("DELETE")
+        config.addAllowedMethod("PATCH")
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
 
     companion object {
 
