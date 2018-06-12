79c26d4e72c3b41142dae417528d28c5b3bce7bc
Titov Artem <tita@1c.ru>
2018-06-12 17:29:55 +0300

    Configure configs for deployment to Heroku for auth-service

--- deploy/create-environment.sh
+++ deploy/create-environment.sh
@@ -7,3 +7,5 @@ sh deploy/create-registered-service.sh graal-hashtag ${EUREKA_URL}
 sh deploy/create-registered-service.sh graal-user ${EUREKA_URL}
 
 sh deploy/create-registered-service.sh graal-post ${EUREKA_URL}
+
+sh deploy/create-registered-service.sh graal-auth ${EUREKA_URL}
