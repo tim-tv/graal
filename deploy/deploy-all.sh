d0a56f0212e63dfda8408f07a9d8fc2c9603a71d
titart <pbi62007@yandex.ru>
2018-06-14 01:02:09 +0300

    Add deploy stage to Travis CI

---
+++ deploy/deploy-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sh deploy/deploy-eureka-service.sh
+sh deploy/deploy-auth-service.sh
+sh deploy/deploy-user-service.sh
+sh deploy/deploy-hashtag-service.sh
+sh deploy/deploy-post-service.sh
+sh deploy/deploy-aggregation-service.sh
+sh deploy/deploy-gateway-service.sh
+sh deploy/deploy-statistic-service.sh
