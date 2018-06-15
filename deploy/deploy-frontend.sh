1c05868953607166a8218eb526b2321c7d7d30b4
Titov Artem <tita@1c.ru>
2018-06-15 15:59:20 +0300

    Fix frontend deploy config

--- deploy/deploy-frontend.sh
+++ deploy/deploy-frontend.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-heroku git:remote --app graal-frontend
-git subtree push --prefix graal-web-app/dist heroku master
 git remote add heroku https://heroku:$HEROKU_API_KEY@git.heroku.com/graal-frontend.git
-
-git push -f heroku master
+git push heroku `git subtree split --prefix graal-web-app/dist master`:master --force
