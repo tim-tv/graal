8505a84d61aeecaec7897e9406e3f233c41eb362
Titov Artem <tita@1c.ru>
2018-06-15 15:37:50 +0300

    Update deployment script for front-end application

--- deploy/deploy-frontend.sh
+++ deploy/deploy-frontend.sh
@@ -2,3 +2,6 @@
 
 heroku git:remote --app graal-frontend
 git subtree push --prefix graal-web-app/dist heroku master
+git remote add heroku https://heroku:$HEROKU_API_KEY@git.heroku.com/graal-frontend.git
+
+git push -f heroku master
